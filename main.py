753fc41f7f83dbe4b5a17ce63737be183b5b57ad
Владислав Быков <vladislav.bykov219@gmail.com>
2023-09-14 20:46:26 +0300

    Delete animations.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from random import randint
-from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -87,8 +86,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-if __name__ == '__main__':
-    run_screensaver()
+def main():
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
